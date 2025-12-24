1f9148bde6ea0391dc77cbf24aa2e17622bd4b1e
Andy Frith <andy_frith@yahoo.com>
2025-12-24 08:40:35 -0700

    feat: Add ActionLink and ActionButton components.

--- components/actions/Actions.tsx
+++ components/actions/Actions.tsx
@@ -1,14 +1,6 @@
+import { ActionLink } from "@/components/actions/ActionLink";
 import { useActions } from "@/hooks/useActions";
-import { Ionicons } from "@expo/vector-icons";
-import { LinearGradient } from "expo-linear-gradient";
-import { Link } from "expo-router";
-import {
-  Dimensions,
-  StyleSheet,
-  Text,
-  TouchableOpacity,
-  View,
-} from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 
 export function Actions({
   title = "Actions",
@@ -28,31 +20,13 @@ export function Actions({
       <Text style={styles.sectionTitle}>{title}</Text>
       <View style={styles.actionsGrid}>
         {actions.map((action) => (
-          <Link href={action.route as any} key={action.label} asChild>
-            <TouchableOpacity style={styles.actionButton}>
-              <LinearGradient
-                colors={["white", "white"]}
-                // colors={action.gradient as [string, string]}
-                style={styles.actionGradient}
-              >
-                <View style={styles.actionContent}>
-                  <View
-                    style={[
-                      styles.actionIcon,
-                      { backgroundColor: `${action.color}15` },
-                    ]}
-                  >
-                    <Ionicons
-                      name={action.icon as keyof typeof Ionicons.glyphMap}
-                      size={28}
-                      color={action.color}
-                    />
-                  </View>
-                  <Text style={styles.actionLabel}>{action.label}</Text>
-                </View>
-              </LinearGradient>
-            </TouchableOpacity>
-          </Link>
+          <ActionLink
+            key={action.label}
+            label={action.label}
+            icon={action.icon}
+            color={action.color}
+            route={action.route as any}
+          />
         ))}
       </View>
     </View>
@@ -69,35 +43,6 @@ const styles = StyleSheet.create({
     flexWrap: "wrap",
     gap: 12,
     marginTop: 15,
-  },
-  actionButton: {
-    width: (Dimensions.get("window").width - 52) / 2,
-    height: 120,
-    borderRadius: 16,
-    overflow: "hidden",
-  },
-  actionGradient: {
-    flex: 1,
-    padding: 15,
-  },
-  actionContent: {
-    flex: 1,
-    justifyContent: "space-between",
-  },
-  actionIcon: {
-    width: 50,
-    height: 50,
-    borderRadius: 25,
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  actionLabel: {
-    // fontFamily: Fonts.brand,
-    fontSize: 16,
-    fontWeight: "600",
-    color: "#333",
-    marginTop: 8,
-    paddingLeft: 10,
   },
   sectionTitle: {
     fontSize: 20,
